0d0f13b810621349fe66a11166ebcd2fa9ed2053
erfan <erfantavakoly@comp.iust.ac.ir>
2018-10-02 10:47:17 +0330

    Add Basic Menu and UI Add functions for Exiting, Resuming and Restarting the game

--- Assets/Scripts/Game/GameHandler.cs
+++ Assets/Scripts/Game/GameHandler.cs
@@ -2,18 +2,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour {
 
     public ScoreManager scoreManager;
     public AsteroidSpawner asteroidSpawner;
     public TimeManager timeManager;
+    public SpaceCraft spaceCraft;
+    public GameUIManager gameUI;
 
-    private void Start()
+    public enum GameStateEnum
     {
+        Menu,
+        Pause,
+        Run
+    }
+
+    private GameStateEnum gameState;
+    public GameStateEnum GameState
+    {
+        get { return gameState; }
+    }
+
+    public void StartNewGame()
+    {
+        Time.timeScale = 1;
         asteroidSpawner.StartSpawning();
         asteroidSpawner.AsteroidDestroyedAction += AsteroidDestroyedCallBack;
         timeManager.StartTimer();
+        gameState = GameStateEnum.Run;
+        spaceCraft.enabled = true;
     }
     private void OnDestroy()
     {
@@ -23,4 +42,27 @@ public class GameHandler : MonoBehaviour {
     {
         scoreManager.DestoryAsteroid();
     }
+
+    public void ExitTheGame()
+    {
+        Application.Quit();
+    }
+    public void PauseTheGame()
+    {
+        Time.timeScale = 0;
+        gameState = GameStateEnum.Pause;
+        spaceCraft.enabled = false;
+
+    }
+    public void ResumeTheGame()
+    {
+        Time.timeScale = 1;
+        gameState = GameStateEnum.Run;
+        spaceCraft.enabled = true;
+    }
+    public void RestartTheGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
 }
---
+++ Assets/Scripts/Game/GameUIManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    public Button startButton;
+    public Button restartButton;
+    public Button resumeButton;
+    public Button exitButton;
+
+    public GameHandler gamehandler;
+    public GameObject menuPanel;
+
+    private static bool showtheMenu = true;
+
+    void Start()
+    {
+      
+        startButton.onClick.AddListener(() => OnStartButtonClicked());
+        resumeButton.onClick.AddListener(() => OnResumeButtonClicked());
+        restartButton.onClick.AddListener(() => OnRestartButtonClicked());
+        exitButton.onClick.AddListener(() => OnExitButtonClicked());
+
+        restartButton.gameObject.SetActive(false);
+        resumeButton.gameObject.SetActive(false);
+
+        if (!showtheMenu)
+        {
+            OnStartButtonClicked();
+        }
+    }
+
+    #region ButtonClickedCallBack
+    private void OnExitButtonClicked()
+    {
+        gamehandler.ExitTheGame();
+    }
+
+    private void OnRestartButtonClicked()
+    {
+        RestartGame();
+    }
+
+    private void OnResumeButtonClicked()
+    {
+        ResumeGame();
+    }
+
+    void OnStartButtonClicked()
+    {
+        gamehandler.StartNewGame();
+        menuPanel.gameObject.SetActive(false);
+
+        startButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
+        resumeButton.gameObject.SetActive(true);
+
+    }
+    #endregion
+
+    public void PauseGame()
+    {
+        menuPanel.gameObject.SetActive(true);
+        
+        gamehandler.PauseTheGame();
+    }
+    public void ResumeGame()
+    {
+        menuPanel.gameObject.SetActive(false);
+
+        gamehandler.ResumeTheGame();
+    }
+    public void RestartGame()
+    {
+        showtheMenu = false;
+        gamehandler.RestartTheGame();
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamehandler.GameState==GameHandler.GameStateEnum.Run)
+                PauseGame();
+            else if (gamehandler.GameState == GameHandler.GameStateEnum.Pause)
+                ResumeGame();
+            
+        }
+    }
+}
